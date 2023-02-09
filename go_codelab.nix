045cd2b91a03e4964bd65045b82e9cfa85d3dc31
GrimReaper <11490705+reaper8055@users.noreply.github.com>
2023-02-09 13:58:09 +0530

    Create go_codelab.nix

---
+++ go_codelab.nix
@@ -0,0 +1,21 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+with pkgs;
+
+mkShell {
+	buildInputs = [
+		go
+		zsh
+    gotools
+    gopls
+    go-outline
+    gocode
+    gopkgs
+    gocode-gomod
+    godef
+    golint
+    rnix-lsp
+	];
+	
+	MY_ENVIRONMENT_VARIABLE = "go-codelabs";
+}
